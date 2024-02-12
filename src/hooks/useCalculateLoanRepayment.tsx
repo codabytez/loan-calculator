4fd554c325735f3b92c4022cebecb64f722830f4
codabytez <chidiobinna0001@gmail.com>
2024-02-12 10:53:46 +0100

    fix: fixed repayment function

--- src/hooks/useCalculateLoanRepayment.tsx
+++ src/hooks/useCalculateLoanRepayment.tsx
@@ -1,21 +1,12 @@
 export function useCalculateMonthlyPayment() {
   return (loanAmount: number, loanDuration: number, interestRate: number) => {
-    const monthlyInterestRate = interestRate / 12 / 100;
-    const monthlyPayment =
-      (loanAmount * monthlyInterestRate) /
-      (1 - Math.pow(1 + monthlyInterestRate, -loanDuration));
-    const totalRepayment = monthlyPayment * loanDuration;
+    const monthlyInterestRate = interestRate / 100;
+
+    const totalRepayment =
+      loanAmount * Math.pow(1 + monthlyInterestRate, loanDuration);
+    const monthlyPayment = totalRepayment / loanDuration;
     return { monthlyPayment, totalRepayment, monthlyInterestRate };
   };
 }
 
-export function useCalculatePaymentTwo() {
-  return (loanAmount: number, loanDuration: number, interestRate: number) => {
-    const monthlyInterestRate = (interestRate * loanDuration) / 12 / 100;
-    const monthlyPayment =
-      (loanAmount * monthlyInterestRate) /
-      (1 - Math.pow(1 + monthlyInterestRate, -loanDuration));
-    const totalRepayment = monthlyPayment * loanDuration;
-    return { monthlyPayment, totalRepayment, monthlyInterestRate };
-  };
-}
+//NOTE: totalRepayment = loanAmount × (1 + monthlyInterestRate) ^ loanDuration
